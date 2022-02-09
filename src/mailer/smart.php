f916daec9e126cc15bd184d4cfd2ca06493c95c7
Ilnur Nurtdinov <nurtdinov.14@gmail.com>
2022-02-09 14:45:09 +0300

    delet mail

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,12 +13,12 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'nurtdinov.14@gmail.com';           // Наш логин
-$mail->Password = 'cgqortbrleqozwkb';                           // Наш пароль от ящика
+$mail->Username = '@gmail.com';           // Наш логин
+$mail->Password = 'cgqort';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('nurtdinov.14@gmail.com', 'Pulse');   // От кого письмо 
+$mail->setFrom('@gmail.com', 'Pulse');   // От кого письмо 
 $mail->addAddress('pedigoj832@chinamkm.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
